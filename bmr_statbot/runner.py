23720e6794861163cd3fa39153a53a854ffe73e1
D Anzorge <d.anzorge@gmail.com>
2013-08-25 00:45:16 +0200

    Add logging

--- bmr_statbot/runner.py
+++ bmr_statbot/runner.py
@@ -20,4 +20,16 @@ def run_scraper(post=True):
 
 
 if __name__ == '__main__':
-    run_scraper(True)
+    import logging
+    
+    logger = logging.getLogger('bmrstatbot')
+    logger.setLevel(logging.INFO)
+    file_handler = logging.FileHandler('bmrstatbot.log')
+    file_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s :: %(levelname)s : %(message)s') 
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    
+    logger.info('Starting bmr_statbot...')
+       
+ #   run_scraper(True)
